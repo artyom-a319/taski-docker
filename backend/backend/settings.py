66f0d25e2e03f18cdd407f82126d44a50bbd484e
Артем Черепанов <dogomsn@gmail.com>
2024-10-28 14:59:56 +0500

    4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_mm0s$h!-p@on1_^'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.158.178.244', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['89.169.167.49', 'artyom.zapto.org', '127.0.0.1']
 
 
 # Application definition
